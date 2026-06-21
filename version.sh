7512c11080c828f301fcf328f53375fe9593a367
yzhyhalo <37714179+yzhyhalo@users.noreply.github.com>
2026-06-21 22:11:16 +0300

    Fix sed command syntax for version updates

--- version.sh
+++ version.sh
@@ -7,10 +7,10 @@
 helm_version=$(cat helm_version)
 app_version=$(cat app_version)
 
-sed  -i "" 's/^version:.*/version: '$helm_version'/g' helm/app/Chart.yaml
-sed  -i "" 's/^  tag:.*/  tag: \"'$app_version'\"/g' helm/app/values.yaml
-sed  -i "" 's/^appVersion:.*/appVersion: \"'$app_version'\"/g' helm/app/Chart.yaml
-sed  -i "" 's/^version = .*/version = \"'$app_version'\"/g' app/Cargo.toml
+sed  -i  's/^version:.*/version: '$helm_version'/g' helm/app/Chart.yaml
+sed  -i  's/^  tag:.*/  tag: \"'$app_version'\"/g' helm/app/values.yaml
+sed  -i  's/^appVersion:.*/appVersion: \"'$app_version'\"/g' helm/app/Chart.yaml
+sed  -i  's/^version = .*/version = \"'$app_version'\"/g' app/Cargo.toml
 
 
 
