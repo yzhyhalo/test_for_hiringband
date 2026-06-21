11dfbb4b8c6833e18758b5da412bee4c9f274406
yzhyhalo <37714179+yzhyhalo@users.noreply.github.com>
2026-06-21 21:06:31 +0300

    Refactor version.sh to simplify version updates

--- version.sh
+++ version.sh
@@ -7,16 +7,11 @@
 helm_version=$(cat helm_version)
 app_version=$(cat app_version)
 
-case "$1" in
-  helm) sed  -i 's/^version:.*/version: '$helm_version'/g' helm/app/Chart.yaml ;;
-  app)  sed  -i 's/^  tag:.*/  tag: \"'$app_version'\"/g' helm/app/values.yaml;\
-    sed  -i 's/^appVersion:.*/appVersion: \"'$app_version'\"/g' helm/app/Chart.yaml;\
-    sed  -i 's/^version = .*/version = \"'$app_version'\"/g' app/Cargo.toml ;;
-  both_helm_app) sed  -i '' 's/^version:.*/version: '$helm_version'/g' helm/app/Chart.yaml;\
-    sed  -i 's/^  tag:.*/  tag: \"'$app_version'\"/g' helm/app/values.yaml;\
-    sed  -i 's/^appVersion:.*/appVersion: \"'$app_version'\"/g' helm/app/Chart.yaml;\
-    sed  -i 's/^version = .*/version = \"'$app_version'\"/g' app/Cargo.toml ;;
-esac
+sed  -i  's/^version:.*/version: '$helm_version'/g' helm/app/Chart.yaml
+sed  -i 's/^  tag:.*/  tag: \"'$app_version'\"/g' helm/app/values.yaml
+sed  -i 's/^appVersion:.*/appVersion: \"'$app_version'\"/g' helm/app/Chart.yaml
+sed  -i 's/^version = .*/version = \"'$app_version'\"/g' app/Cargo.toml
+
 
 
 
