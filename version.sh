0adda711a3b55d99bbefb10ddd428f9682c9e628
Yevhen <yevhen@MacBook-Pro-admin.local>
2026-06-22 16:47:20 +0300

    fix shell

--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@
 helm_version=$(cat helm_version)
 app_version=$(cat app_version)
 
-sed -E  "s/^version:.*/version: $helm_version/g" -I helm/app/Chart.yaml >helm/app/Chart.yaml 
-sed -E  "s/^  tag:.*/  tag: \"$app_version\"/g" -I helm/app/values.yaml >helm/app/values.yaml 
-sed -E "s/^appVersion:.*/appVersion: \"$app_version\"/g" -I  helm/app/Chart.yaml  > helm/app/Chart.yaml
-sed -E  "s/^version = .*/version = \'$app_version\'/g" -I app/Cargo.toml >app/Cargo.toml  
+sed -i ''  "s/^version:.*/version: $helm_version/g" helm/app/Chart.yaml
+sed -i ''  "s/^  tag:.*/  tag: \"$app_version\"/g" helm/app/values.yaml 
+sed -i ''  "s/^appVersion:.*/appVersion: \"$app_version\"/g" helm/app/Chart.yaml
+sed -i ''  "s/^version = .*/version = \'$app_version\'/g" app/Cargo.toml 
