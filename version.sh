0625a1b230a0baa892fb9e50abfa55cf3a833878
Yevhen <yevhen@MacBook-Pro-admin.local>
2026-06-22 15:36:21 +0300

    fix shell scripts

--- version.sh
+++ version.sh
@@ -7,13 +7,7 @@
 helm_version=$(cat helm_version)
 app_version=$(cat app_version)
 
-case "$1" in
-  helm) sed  -i 's/^version:.*/version: '$helm_version'/g' helm/app/Chart.yaml ;;
-  app)  sed  -i 's/^  tag:.*/  tag: \"'$app_version'\"/g' helm/app/values.yaml;\
-    sed  -i 's/^appVersion:.*/appVersion: \"'$app_version'\"/g' helm/app/Chart.yaml;\
-    sed  -i 's/^version = .*/version = \"'$app_version'\"/g' app/Cargo.toml ;;
-  both_helm_app) sed  -i '' 's/^version:.*/version: '$helm_version'/g' helm/app/Chart.yaml;\
-    sed  -i 's/^  tag:.*/  tag: \"'$app_version'\"/g' helm/app/values.yaml;\
-    sed  -i 's/^appVersion:.*/appVersion: \"'$app_version'\"/g' helm/app/Chart.yaml;\
-    sed  -i 's/^version = .*/version = \"'$app_version'\"/g' app/Cargo.toml ;;
-esac
+sed -IE  "s/^version:.*/version: $helm_version/g" helm/app/Chart.yaml ;\
+sed -IE  "s/^  tag:.*/  tag: \"$app_version\"/g" helm/app/values.yaml ;\
+sed -IE "s/^appVersion:.*/appVersion: \"$app_version\"/g" helm/app/Chart.yaml ;\
+sed -IE  "s/^version = .*/version = \'$app_version\'/g" app/Cargo.toml 
